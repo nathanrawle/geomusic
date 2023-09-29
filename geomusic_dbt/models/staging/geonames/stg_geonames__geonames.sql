f5f9d057c8e0d80430f74271a08ccaef922cbceb
Nathan Rawle <nathan.rawle@gmail.com>
2023-09-29 11:54:30 +0100

    refactor[dbt]: put an index on geonames

--- geomusic_dbt/models/staging/geonames/stg_geonames__geonames.sql
+++ geomusic_dbt/models/staging/geonames/stg_geonames__geonames.sql
@@ -1,1 +1,10 @@
+{{
+    config(
+        materialized = "materialized_view",
+        indexes = [
+            {"columns": ["geoname_id"], "unique": True}
+        ]
+    )
+}}
+
 SELECT * FROM {{ source( 'geonames', 'geonames' ) }}
