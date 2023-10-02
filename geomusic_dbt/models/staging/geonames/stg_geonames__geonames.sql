d022198155094f39a81b6fe47b99340317023782
Nathan Rawle <nathan.rawle@gmail.com>
2023-10-02 21:49:27 +0100

    refactor[dbt]: make geonames incremental

--- geomusic_dbt/models/staging/geonames/stg_geonames__geonames.sql
+++ geomusic_dbt/models/staging/geonames/stg_geonames__geonames.sql
@@ -1,10 +1,21 @@
 {{
     config(
-        materialized = "materialized_view",
+        materialized = "incremental",
+        unique_key = "geoname_id",
         indexes = [
             {"columns": ["geoname_id"], "unique": True}
         ]
     )
 }}
 
-SELECT * FROM {{ source( 'geonames', 'geonames' ) }}
+SELECT *
+
+FROM {{ source( 'geonames', 'geonames' ) }}
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  -- (uses > to include records whose timestamp occurred since the last run of this model)
+  where gn_modification_date > (select max(gn_modification_date) from {{ this }})
+
+{% endif %}
